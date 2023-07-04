773c075a669be5fca5c7aed7455af6579fefe155
Rod Kay <rodakay5@gmail.com>
2023-07-04 14:31:43 +1000

    testsuite.do_tidy: Simplify.

--- Examples/ada/testsuite/do_tidy.sh
+++ Examples/ada/testsuite/do_tidy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-set -e
-
-#pwd
-ls     ../../test-suite/*_wrap.c   \
-       ../../test-suite/*_wrap.cxx 
-       
 rm -fr ../../test-suite/*_wrap.c
 rm -fr ../../test-suite/*_wrap.cxx
